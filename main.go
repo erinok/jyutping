cabae6138f16753aaac69f7d3fe1d1330d32f2c2
Erin Keenan <erinok@gmail.com>
2018-09-06 23:47:43 -0700

    tiny cleanup

--- main.go
+++ main.go
@@ -9,43 +9,44 @@ import (
 	"github.com/erinok/jyutping/lu"
 )
 
-func convert(s string, sp bool) {
-	if s == "" {
-		return
-	}
-	if sp { 
-		fmt.Print(" ")
-	}
-	t := ""
-	j := 0
-	i := 1
-	for {
-		t_, prefix := lu.M[s[:i]]
-		if !prefix {
-			break
+func convert(s string) {
+	sp := false
+	for s != "" {
+		if sp {
+			fmt.Print(" ")
 		}
-		if t_ != "" {
-			t = t_
-			j = i
+		sp = true
+		t := ""
+		j := 0
+		i := 1
+		for {
+			t_, prefix := lu.M[s[:i]]
+			if !prefix {
+				break
+			}
+			if t_ != "" {
+				t = t_
+				j = i
+			}
+			if i == len(s) {
+				break
+			}
+			i++
 		}
-		if i == len(s) {
-			break
+		if t != "" {
+			fmt.Print(t)
+			s = s[j:]
+		} else {
+			fmt.Print(s[:i])
+			s = s[i:]
 		}
-		i++
-	}
-	if t != "" {
-		fmt.Print(t)
-		convert(s[j:], true)
-	} else {
-		fmt.Print(s[:i])
-		convert(s[i:], true)
 	}
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		convert(scanner.Text(), false)
+		convert(scanner.Text())
 	}
 	fmt.Println()
 }
